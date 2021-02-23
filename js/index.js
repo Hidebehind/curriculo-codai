ddccc85080b2602258f2b1d9e2f3d0b8f396f662
Hidebehind <alan.rs@outlook.com>
2021-02-23 03:09:42 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
+var texto2 = document.getElementById('texto2');
+var texto1 = document.getElementById('texto1');
 
+test();
+function test() {
+    texto2.style.display = "none";
+}
+
+function clickProjetos(){
+    console.log("Clicou projetos");
+    texto1.style.display = "none";
+    texto2.style.display = "block";
+    console.log("deu certo");
+}
+
+function clickSobre() {
+    console.log("Clicou sobre");
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
